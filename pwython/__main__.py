3607cc0be5de4d48fab90feae532234931f4a362
SystematicError <systematicerror@users.noreply.github.com>
2021-09-16 00:10:15 +0530

    Initial Commit

---
+++ pwython/__main__.py
@@ -0,0 +1,29 @@
+from random import choice
+from re import sub
+from subprocess import PIPE, Popen
+from sys import argv, executable, stderr
+
+
+def owoify(text):
+    if type(text) == bytes:
+        text = str(text)[2:-1].replace("\\n", "\n")
+
+    text = sub("[rlv]", "w", text)
+    text = sub("[RLV]", "W", text)
+    text = sub("ee", "wee", text)
+
+    return text
+
+
+pronouns = open("pwython/pronouns.txt").read().splitlines()
+remarks = open("pwython/remarks.txt").read().splitlines()
+reactions = open("pwython/reactions.txt").read().splitlines()
+
+process = Popen([executable] + argv[1:], stderr=PIPE)
+
+while process.poll() is None:
+    for line in iter(process.stderr.readline, b""):
+        if line == b"Traceback (most recent call last):\n":
+            stderr.write(f"{choice(pronouns)}, {choice(remarks)} {choice(reactions)}\n")
+
+        stderr.write(owoify(line))
