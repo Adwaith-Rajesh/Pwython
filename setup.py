1efd22651ee8a821894e33efd1362e9b5e0698c4
SystematicError <systematicerror@users.noreply.github.com>
2021-09-16 20:00:21 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pwython",
-    version="dev",
+    version="1.0.0",
     description="A meme error handler for python",
     author="Systematic Error",
     url="https://github.com/SystematicError/Pwython",
