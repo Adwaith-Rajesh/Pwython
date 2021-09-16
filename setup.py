9105bc9f5a3da8db02121d0b35de4e5841b97255
SystematicError <systematicerror@users.noreply.github.com>
2021-09-16 09:25:31 +0530

    Add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pwython",
+    version="dev",
+    description="A meme error handler for python",
+    author="Systematic Error",
+    url="https://github.com/SystematicError/Pwython",
+    packages=["pwython"]
+)
